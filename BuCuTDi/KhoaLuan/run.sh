74e3895b23c85a9b68f3f62c1c9103c47d2724ca
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2024-11-13 23:09:36 -0500

    XUATTINH

---
+++ BuCuTDi/KhoaLuan/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo $Home
+
+echo 'args:' $@
+
+# with control
+#python run_object_sketching.py $@ --num_strokes 16 --num_segments 5 --num_sketches 5 --attention_init 0 --text_prompt "a_simple_drawing_of_a_bicycle" --sketches_edit "input/wheel.png" --output_name "bicycle_w" --train_with_diffusion --control
+
+# without control (VectorFusion)
+python run_object_sketching.py $@ --num_strokes 16 --num_segments 5 --num_sketches 5 --attention_init 0 --text_prompt "a_simple_drawing_of_a_bicycle" --output_name "bicycle_o" --train_with_diffusion
